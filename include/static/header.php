62e0175f76e0453ff04dcfe4f68df2c910b10694
97gushan <97gustavh@gmail.com>
2016-08-13 23:16:21 +0200

    added a footer and header file
    The header and footer on every page is going to be the same so they are
    separated and placed in different files.

---
+++ include/static/header.php
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="assets/css/style.css" />
+        <meta charset="utf-8" />
+        <title>Prototype</title>
+
+
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <h1 class="main_header">PROTOTYPE</h1>
+            </div>
