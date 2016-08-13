e3d5bfbcac306a9bbcd375ea8cf6484ee080ccce
97gushan <97gustavh@gmail.com>
2016-08-13 23:35:21 +0200

    put the form into a div

--- register_page.php
+++ register_page.php
@@ -2,15 +2,17 @@
     include("include/static/header.php");
 ?>
 
-    <form method="post" action="include/database/register_user.php">
-        <input type="text" id="name" placeholder="Ange namn" />
-        <input type="pass" id="pass" placeholder="Ange lösenord" />
-        <input type="text" id="mail" placeholder="Ange mailadress" />
-        <input type="text" id="persnum" placeholder="Ange personnummer" />
+    <div id="form">
+        <form method="post" action="include/database/register_user.php">
+            <input type="text" id="name" placeholder="Ange namn" />
+            <input type="pass" id="pass" placeholder="Ange lösenord" />
+            <input type="text" id="mail" placeholder="Ange mailadress" />
+            <input type="text" id="persnum" placeholder="Ange personnummer" />
 
-        <input type="submit" id="submit" name="submit" value="Registrera dig" />
+            <input type="submit" id="submit" name="submit" value="Registrera dig" />
 
-    </form>
+        </form>
+    </div>
 
 <?php
     include("include/static/footer.php");
