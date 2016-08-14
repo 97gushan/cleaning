2d253301a68da43f7889915edf17c65845a5546a
97gushan <97gustavh@gmail.com>
2016-08-14 22:05:40 +0200

    get the values from the user and crypt the pass
    The file now prepares variables and cryptates the password so they are
    ready to be used in the DB later on

--- include/database/register_user.php
+++ include/database/register_user.php
@@ -1,7 +1,13 @@
 
 <?php
 
-    
+    $name = filter_input(INPUT_GET, "name", FILTER_SANITIZE_STRING);
+    $mail = filter_input(INPUT_GET, "mail", FILTER_SANITIZE_STRING);
+    $persnum = filter_input(INPUT_GET, "persnum", FILTER_SANITIZE_STRING);
+    $pass = filter_input(INPUT_GET, "pass", FILTER_SANITIZE_STRING);
+    $salt = filter_input(INPUT_GET, "salt", FILTER_SANITIZE_STRING);
+
+    $crypted_pass = crypt($pass, $salt);
 
 
 ?>
