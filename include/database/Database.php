992735296a298b4b33b0611c9eda4955ee1d5132
97gushan <97gustavh@gmail.com>
2016-08-14 22:39:12 +0200

    Created register_user method
    this method starts with connecting to the DB, then create the sql
    command. The command is then prepared and if it is sucessfull the
    command is executed and a user is registered

--- include/database/Database.php
+++ include/database/Database.php
@@ -3,7 +3,7 @@
     class Database{
 
         private $conn;
-        private $db = "temp";
+        private $db = "skurat";
 
         private $host = "localhost";
         private $user = "root";
@@ -12,10 +12,32 @@
         private function Connect(){
             $this->conn = new mysqli($this->host, $this->user,$this->pass,$this->db);
 
-            if($this->conn->error()){
+            if($this->conn->error){
                 $error_message = $this->conn->connect_errno;
 
                 die("Error: $error_message");
+            }
+        }
+
+        public function register_user($name, $mail, $persnum, $pass, $salt){
+
+            $this->Connect();
+
+            $sql = "INSERT INTO user(name, mail, pass, salt, persnum) VALUES(?,?,?,?,?)";
+
+            if($stmt = $this->conn->prepare($sql)){
+                $stmt->bind_param("sssss", $name, $mail, $pass, $salt, $persnum);
+
+                $stmt->execute();
+
+                if($stmt->error){
+                    return false;
+                }
+
+                $stmt->close();
+                $this->conn->close();
+
+                return true;
             }
         }
 
