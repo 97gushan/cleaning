5c1e708bc5ac7cbecbc420b5c3148f6cab069d8e
97gushan <97gustavh@gmail.com>
2016-08-15 00:53:56 +0200

    check if the entered pass is equal to the stored

---
+++ include/database/login_user.php
@@ -0,0 +1,23 @@
+<?php
+
+    include("Database.php");
+
+    $mail = filter_input(INPUT_GET, "mail", FILTER_VALIDATE_EMAIL);
+    $pass = filter_input(INPUT_GET, "pass", FILTER_SANITIZE_STRING);
+
+    $db = new Database();
+
+    $saved_data = $db->get_user_credentials($mail);
+
+    $saved_pass = $saved_data[0];
+    $salt = $saved_data[1];
+
+    $crypted_pass = crypt($pass, $salt);
+
+    if(strcmp($crypted_pass, $saved_pass) == 0){
+        echo true;
+    }else{
+        echo false;
+    }
+
+?>
