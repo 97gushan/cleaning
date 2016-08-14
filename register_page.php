6d09ce3d1fbf3a18dc1a76da80aba647ca728825
97gushan <97gustavh@gmail.com>
2016-08-14 23:55:40 +0200

    add some minor styling

--- register_page.php
+++ register_page.php
@@ -4,9 +4,18 @@
 
     <div id="form">
         <form >
+            <h2>Registrera dig</h2>
+
+            <div id="id_field">
+                <input type="text" class="text_field" id="persnum" placeholder="Ange personnummer" />
+                <p class="text_field_description">(ååmmdd)</p>
+            </div>
+
             <input type="text" class="text_field" id="name" placeholder="Ange namn" />
             <input type="text" class="text_field" id="mail" placeholder="Ange mailadress" />
-            <input type="text" class="text_field" id="persnum" placeholder="Ange personnummer" />
+
+
+
             </br>
             <input type="password" class="text_field" id="pass" placeholder="Ange lösenord" />
             <input type="password" class="text_field" id="pass_2" placeholder="Ange lösenordet igen" />
@@ -14,6 +23,7 @@
 
             <p style="clear: left;"></p>
 
+            <!--Generate a salt-->
             <?php
                 $salt = "";
 
