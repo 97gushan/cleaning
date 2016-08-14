f72895a6bfc15860eb66d5509245fea2d9ebd15e
97gushan <97gustavh@gmail.com>
2016-08-15 00:52:19 +0200

    added id to the input tags

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 
     <div id="form">
         <form method="post" action="login.php">
-            <input type="text" placeholder="Ange användarnamn" />
-            <input type="password" placeholder="Ange lösenord" />
-            <input type="submit" value="Login" name="submit" />
+            <input type="text" id="mail" placeholder="Ange mailadress" />
+            <input type="password" id="pass" placeholder="Ange lösenord" />
+            <input type="button" onclick="_send_data.login_user();" />
         </form>
 
         <a href="register_page.php">Registrera dig</a>
