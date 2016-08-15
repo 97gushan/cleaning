0cab4c10b01a09769d6044214d2d9d8b30985ed4
97gushan <97gustavh@gmail.com>
2016-08-15 12:16:45 +0200

    moved login form to its own file

--- index.php
+++ index.php
@@ -4,18 +4,9 @@
     <!-- TODO check if the user is logged in, if yes then display the
         ratin page-->
 
-    <div id="form">
-        <form method="post" action="login.php">
-            <input type="text" id="mail" placeholder="Ange mailadress" />
-            <input type="password" id="pass" placeholder="Ange lösenord" />
-            <input type="button" onclick="_send_data.login_user();" />
-        </form>
-
-        <a href="register_page.php">Registrera dig</a>
-    </div>
-
-
-
 <?php
+    include("login_page.php");
+
+
     include("include/static/footer.php");
 ?>
---
+++ login_page.php
@@ -0,0 +1,9 @@
+<div id="form">
+    <form method="post" action="login.php">
+        <input type="text" id="mail" placeholder="Ange mailadress" />
+        <input type="password" id="pass" placeholder="Ange lösenord" />
+        <input type="button" onclick="_send_data.login_user();" />
+    </form>
+
+    <a href="register_page.php">Registrera dig</a>
+</div>
