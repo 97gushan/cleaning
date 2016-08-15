be94852defcb1e4229df7f3676aeaba5f4fd5238
97gushan <97gustavh@gmail.com>
2016-08-15 12:59:26 +0200

    Added text to the login button

--- include/views/login_page.php
+++ include/views/login_page.php
@@ -2,7 +2,7 @@
     <form method="post">
         <input type="text" id="mail" placeholder="Ange mailadress" />
         <input type="password" id="pass" placeholder="Ange lösenord" />
-        <input type="button" onclick="_send_data.login_user();" />
+        <input type="button" value="Logga in" onclick="_send_data.login_user();" />
     </form>
 
     <a href="register_page.php">Registrera dig</a>
