430ff17ace7c4f53ed2472289e0405327644a035
97gushan <97gustavh@gmail.com>
2016-08-15 12:35:09 +0200

    check if the user is logged in
    First of all the $_SESSION["login"] is checked if it exists or not. If
    yes then the value is checked and if it is "USER" the rating_page is
    shown, else the login_page is shown

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
+    session_start();
+
     include("include/static/header.php");
 ?>
     <!-- TODO check if the user is logged in, if yes then display the
         ratin page-->
 
 <?php
-    include("login_page.php");
+    if(isset($_SESSION["login"])){
+        if($_SESSION["login"] == "USER"){
+            include("rating_page.php");
+        }
+    }else{
+        include("login_page.php");
+    }
+
 
 
     include("include/static/footer.php");
