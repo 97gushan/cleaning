f275a92d46e351030b152871835d0fc65e3486f4
user <user@localhost.localdomain>
2016-09-12 17:26:23 +0100

    Testing regular user commit

--- include/database/reset_pass.php
+++ include/database/reset_pass.php
@@ -1,23 +1,17 @@
 <?php
 
-    require "Z:\Program\MAMP\libs\PHPMailer\PHPMailerAutoload.php";
-
     $given_mail = filter_input(INPUT_GET, "mail", FILTER_VALIDATE_EMAIL);
-
+    /*
     $pass = "";
     for($x = 0; $x < 7; $x++){
         $rand_num = rand(0,26) + 97;
         $pass = $pass . chr($rand_num);
     }
+    */
 
-    $mail = new PHPMailer();
-    $mail->IsSMTP();
-    $mail->Mailer = "smtp";
+    mail("97gustavh@gmail.com", $given_mail, "Text text etxxt text tex", "From: din_son");
 
-
-    #mail($mail, "Glömt lösenord", $pass, "From: Testing");
-
-    #header("Refresh:0; url='../../index.php'");
+    header("Refresh:0; url='../../index.php'");
 
 
  ?>
