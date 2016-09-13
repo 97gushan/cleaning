b763e8a7e48393369a6e4357648c91b26af621a7
97gushan <97gustavh@gmail.com>
2016-09-13 22:31:03 +0200

    Fixing the reseting of the pass
    Also a bit of restructuring

--- include/database/reset_pass.php
+++ include/database/reset_pass.php
@@ -1,35 +1,34 @@
 <?php
     include("Database.php");
 
-    $given_mail = $_POST["mail"];
+	$given_mail = $_POST["mail"];
 
     $pass = "";
     for($x = 0; $x < 7; $x++){
-        $rand_num = rand(0,26) + 97;
-        $pass = $pass . chr($rand_num);
+    	$rand_num = rand(0,26) + 97;
+    	$pass = $pass . chr($rand_num);
     }
 
     $db = new Database();
 
     if($db->is_mail_unique($given_mail)){
-        # send the mail
 
-        $salt = "";
+    	echo "Email does not exist";
 
-        for($x = 0; $x < 10; $x++){
-            $rand_num = rand(0,26) + 97;
-            $salt = $salt . chr($rand_num);
-        }
+    }else{
+    	$salt = "";
 
-        $crypt_pass = crypt($pass, $salt);
+    	for($x = 0; $x < 10; $x++){
+        	$rand_num = rand(0,26) + 97;
+        	$salt = $salt . chr($rand_num);
+    	}
 
-        mail($given_mail, "Nytt lösenord", $pass, "From: reset_pass");
+    	$crypt_pass = crypt($pass, $salt);
 
-        if($db->new_pass($crypt_pass,$salt,$given_mail)){
-            header("Refresh:0; url='../../index.php'");
-        }
+    	mail($given_mail, "Nytt lösenord", $pass, "From: reset_pass");
 
+    	if($db->new_pass($crypt_pass,$salt,$given_mail)){
+        	header("Refresh:0; url='../../index.php'");
+    	}
     }
-
-
- ?>
+?>
