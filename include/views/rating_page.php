51b06cb4a7b1740dcb621fabc84b1409e305af3e
97gushan <97gustavh@gmail.com>
2016-09-14 15:30:33 +0200

    beggin work on the form

--- include/views/rating_page.php
+++ include/views/rating_page.php
@@ -5,3 +5,13 @@
 </form>
 
 <a href=" change_pass_page.php">Byt lösenord</a>
+
+
+<form>
+<?php
+    for($x = 0; $x<10; $x++){
+        echo "<input type='text' id='text$x' placeholder='Wub'/>
+              <input type='text' id='text$x' placeholder='Hub'/>";
+    }
+?>
+</form>
